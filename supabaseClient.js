6dafc43e2be6abd2c3e0f241d1934398b7fd73ac
Pierre ESCOURROU <escourrou.pierre@gmail.com>
2025-07-24 23:22:49 +0200

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://achjuopckjmcqtpeolob.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjaGp1b3Bja2ptY3F0cGVvbG9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyNjEwNDEsImV4cCI6MjA2ODgzNzA0MX0.KeGwez6dlTkwsImgSiaH3qlZBZTgehbXw_MWW2uH8LY';
+// Les variables doivent être définies dans Netlify et/ou un fichier .env local
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// On crée le client Supabase avec les valeurs sécurisées
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
